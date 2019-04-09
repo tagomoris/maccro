f0955dc658bb44605958dff10fc75da927db5094
TAGOMORI Satoshi <tagomoris@gmail.com>
2019-04-09 20:28:34 +0900

    add test cases for built-in rules

---
+++ test/builtin_examples.rb
@@ -0,0 +1,97 @@
+class BuiltinExamples
+  # less_than_2: ['e1 < e2 < e3', '(e1 < e2 && e2 < e3)'],
+  def m_less_than_2(x, y)
+    if x < 1 < y && true
+      true
+    else
+      false
+    end
+  end
+
+  # less_than_or_equal_to_2: ['e1 <= e2 <= e3', '(e1 <= e2 && e2 <= e3)'],
+  # greater_than_2: ['e1 > e2 > e3', '(e1 > e2 && e2 > e3)'],
+  # greater_than_or_equal_to_2: ['e1 >= e2 >= e3', '(e1 >= e2 && e2 >= e3)'],
+
+  # less_than_3: ['e1 < e2 < e3 < e4', '(e1 < e2 && e2 < e3 && e3 < e4)'],
+  # less_than_or_equal_to_3: ['e1 <= e2 <= e3 <= e4', '(e1 <= e2 && e2 <= e3 && e3 <= e4)'],
+  # greater_than_3: ['e1 > e2 > e3 > e4', '(e1 > e2 && e2 > e3 && e3 > e4)'],
+  # greater_than_or_equal_to_3: ['e1 >= e2 >= e3 >= e4', '(e1 >= e2 && e2 >= e3 && e3 >= e4)'],
+
+  # less_than_4: ['e1 < e2 < e3 < e4 < e5', '(e1 < e2 && e2 < e3 && e3 < e4 && e4 < e5)'],
+  # less_than_or_equal_to_4: ['e1 <= e2 <= e3 <= e4 <= e5', '(e1 <= e2 && e2 <= e3 && e3 <= e4 && e4 <= e5)'],
+  # greater_than_4: ['e1 > e2 > e3 > e4 > e5', '(e1 > e2 && e2 > e3 && e3 > e4 && e4 > e5)'],
+  # greater_than_or_equal_to_4: ['e1 >= e2 >= e3 >= e4', '(e1 >= e2 && e2 >= e3 && e3 >= e4 && e4 >= e5)'],
+
+  # open_interval: ['e1 < e2 < e3', '(e1 < e2 && e2 < e3)'],
+  # closed_interval: ['e1 <= e2 <= e3', '(e1 <= e2 && e2 <= e3)'],
+  # left_closed_interval: ['e1 <= e2 < e3', '(e1 <= e2 && e2 < e3)'],
+  # right_closed_interval: ['e1 < e2 <= e3', '(e1 < e2 && e2 <= e3)'],
+
+  # ar_where_equal_to: ['v1 == e1', '{v1 => e1}', {under: 'e1.where($TARGET)'}],
+  def m_ar_where_equal_to(i)
+    Rows.where(:col1 == i)
+  end
+
+  # :ar_where_not_equal_to, ['v1 != e1', '"#{v1} != ?", e1', {under: 'e1.where($TARGET)'}],
+  def m_ar_where_not_equal_to(i)
+    Rows.where(:col1 != i)
+  end
+
+  # :ar_where_larger_than, ['v1 > e1', '"#{v1} > ?", e1', {under: 'e1.where($TARGET)'}],
+  # :ar_where_larger_than_or_equal_to, ['v1 >= e1', '"#{v1} >= ?", e1', {under: 'e1.where($TARGET)'}],
+  # :ar_where_less_than, ['v1 < e1', '"#{v1} < ?", e1', {under: 'e1.where($TARGET)'}],
+  # :ar_where_less_than_or_equal_to, ['v1 <= e1', '"#{v1} <= ?", e1', {under: 'e1.where($TARGET)'}],
+
+  # :ar_where_in_range_exclusive, ['e1 <= v1 < e2', 'v1 => [e1...e2]', {under: 'e1.where($TARGET)'}],
+  def m_ar_where_in_range_exclusive(x, y)
+    Rows.where(x <= :id < y)
+  end
+
+  # :ar_where_in_range_inclusive, ['e1 <= v1 <= e2', 'v1 => [e1..e2]', {under: 'e1.where($TARGET)'}],
+  def m_ar_where_in_range_inclusive(x, y)
+    Rows.where(x <= :id <= y)
+  end
+
+  # :ar_and_chain, ['e1.where(e2 and e3)', 'e1.where(e2).where(e3)'],
+  def m_ar_and_chain_1
+    Rows.where(:x && :y)
+  end
+
+  def m_ar_and_chain_2
+    Rows.where((:x and :y and :z))
+  end
+
+  # :ar_or_chain, ['e1.where(e2 or e3)', 'e1.where(e2).or(e1.where(e3))'],
+  def m_ar_or_chain_1
+    Rows.where(:x || :y)
+  end
+
+  def m_ar_or_chain_2
+    Rows.where((:x or :y or :z))
+  end
+
+  def m_ar_complex_1
+    Rows.where(
+      min <= :id < max &&
+      :name != my_name &&
+      :age > my_age
+    )
+  end
+
+  def m_ar_complex_2
+    Rows.where(
+      :name == "MyName" ||
+      :level >= 2
+    )
+  end
+
+  ## TODO: find a way to implement this
+  #
+  # def m_ar_complex_3
+  #   Rows.where(:age >= 30 && (:level >= 2 || :type == :admin))
+  # end
+  #
+  # def m_ar_complex_4
+  #   Rows.where(:age >= 30 || (:level >= 2 && :type == :admin))
+  # end
+end
---
+++ test/test_builtin.rb
@@ -0,0 +1,135 @@
+require_relative "helper"
+require "maccro/builtin"
+
+class BuiltinTestCase < ::Test::Unit::TestCase
+  suppress_warning do
+    require_relative "builtin_examples"
+  end
+
+  sub_test_case 'continuing less/greater-than or equal-to' do
+    test 'less_than_2' do
+      rules = Maccro::Builtin.rules(:less_than_2)
+      Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_less_than_2), rules: rules)
+      assert BuiltinExamples.new.m_less_than_2(0, 2)
+    end
+  end
+
+  sub_test_case 'mathematic intervals' do
+  end
+
+  sub_test_case 'ActiveRecord utilities' do
+    test 'ar_where_equal_to' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_where_equal_to), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_where_equal_to(i)
+    Rows.where({:col1 => i})
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_where_not_equal_to' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_where_not_equal_to), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_where_not_equal_to(i)
+    Rows.where(["\#{:col1} != ?", i])
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_where_in_range_exclusive' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_where_in_range_exclusive), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_where_in_range_exclusive(x, y)
+    Rows.where({:id => [(x)...(y)]})
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_where_in_range_inclusive' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_where_in_range_inclusive), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_where_in_range_inclusive(x, y)
+    Rows.where({:id => [(x)..(y)]})
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_where_and_chain, once' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_and_chain_1), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_and_chain_1
+    Rows.where(:x).where(:y)
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_where_and_chain, twice' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_and_chain_2), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_and_chain_2
+    Rows.where(:x).where(:y).where(:z)
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_where_or_chain, once' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_or_chain_1), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_or_chain_1
+    Rows.where(:x).or(Rows.where(:y))
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_where_or_chain, twice' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_or_chain_2), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_or_chain_2
+    Rows.where(:x).or(Rows.where(:y)).or(Rows.where(:z))
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_complex: and-connected' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_complex_1), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_complex_1
+    Rows.where({:id => [(min)...(max)]}).where(["\#{:name} != ?", my_name]).where(["\#{:age} > ?", my_age])
+  end
+CODE
+      assert_equal expected_code, code
+    end
+
+    test 'ar_complex: or-connected' do
+      rules = Maccro::Builtin.rules(:activerecord_utilities)
+      code = Maccro.apply(BuiltinExamples, BuiltinExamples.instance_method(:m_ar_complex_2), rules: rules, get_code: true)
+      expected_code = <<CODE.chomp
+  def m_ar_complex_2
+    Rows.where({:name => "MyName"}).or(Rows.where(["\#{:level} >= ?", 2]))
+  end
+CODE
+      assert_equal expected_code, code
+    end
+  end
+end
+
+
+
+
