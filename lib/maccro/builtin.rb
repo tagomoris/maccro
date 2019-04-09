da0d86a6d45390e32b30321136a17eb8526f491a
TAGOMORI Satoshi <tagomoris@gmail.com>
2019-04-09 20:27:03 +0900

    add built-in rules for ActiveRecord queries (demo)

--- lib/maccro/builtin.rb
+++ lib/maccro/builtin.rb
@@ -29,6 +29,27 @@ module Maccro
       closed_interval: ['e1 <= e2 <= e3', '(e1 <= e2 && e2 <= e3)'],
       left_closed_interval: ['e1 <= e2 < e3', '(e1 <= e2 && e2 < e3)'],
       right_closed_interval: ['e1 < e2 <= e3', '(e1 < e2 && e2 <= e3)'],
+
+      # ActiveRecord utilities
+      ar_where_in_range_exclusive: ['e1 <= v1 < e2', '{v1 => [(e1)...(e2)]}', {under: 'e1.where($TARGET)'}],
+      ar_where_in_range_inclusive: ['e1 <= v1 <= e2', '{v1 => [(e1)..(e2)]}', {under: 'e1.where($TARGET)'}],
+      ar_where_equal_to: ['v1 == e1', '{v1 => e1}', {under: 'e1.where($TARGET)'}],
+      ar_where_not_equal_to: ['v1 != e1', '["#{v1} != ?", e1]', {under: 'e1.where($TARGET)'}],
+      ar_where_larger_than: ['v1 > e1', '["#{v1} > ?", e1]', {under: 'e1.where($TARGET)'}],
+      ar_where_larger_than_or_equal_to: ['v1 >= e1', '["#{v1} >= ?", e1]', {under: 'e1.where($TARGET)'}],
+      ar_where_less_than: ['v1 < e1', '["#{v1} < ?", e1]', {under: 'e1.where($TARGET)'}],
+      ar_where_less_than_or_equal_to: ['v1 <= e1', '["#{v1} <= ?", e1]', {under: 'e1.where($TARGET)'}],
+      # TODO: and-or mixed query
+      ar_and_chain_5: ['e1.where((e2 && e3 and e4 and e5 and e6 and e7))', 'e1.where(e2).where(e3).where(e4).where(e5).where(e6).where(e7)'],
+      ar_and_chain_4: ['e1.where((e2 and e3 and e4 and e5 and e6))', 'e1.where(e2).where(e3).where(e4).where(e5).where(e6)'],
+      ar_and_chain_3: ['e1.where((e2 and e3 and e4 and e5))', 'e1.where(e2).where(e3).where(e4).where(e5)'],
+      ar_and_chain_2: ['e1.where((e2 and e3 and e4))', 'e1.where(e2).where(e3).where(e4)'],
+      ar_and_chain_1: ['e1.where((e2 and e3))', 'e1.where(e2).where(e3)'],
+      ar_or_chain_5: ['e1.where((e2 or e3 or e4 or e5 or e6 or e7))', 'e1.where(e2).or(e1.where(e3)).or(e1.where(e4)).or(e1.where(e5)).or(e1.where(e6)).or(e1.where(e7))'],
+      ar_or_chain_4: ['e1.where((e2 or e3 or e4 or e5 or e6))', 'e1.where(e2).or(e1.where(e3)).or(e1.where(e4)).or(e1.where(e5)).or(e1.where(e6))'],
+      ar_or_chain_3: ['e1.where((e2 or e3 or e4 or e5))', 'e1.where(e2).or(e1.where(e3)).or(e1.where(e4)).or(e1.where(e5))'],
+      ar_or_chain_2: ['e1.where((e2 or e3 or e4))', 'e1.where(e2).or(e1.where(e3)).or(e1.where(e4))'],
+      ar_or_chain_1: ['e1.where((e2 or e3))', 'e1.where(e2).or(e1.where(e3))'],
     }.freeze
 
     RULE_GROUPS = {
@@ -42,6 +63,14 @@ module Maccro
       inequality_operators_4: [:less_than_4, :less_than_or_equal_to_4, :greater_than_4, :greater_than_or_equal_to_4],
 
       mathematic_intervals: [:open_interval, :closed_interval, :left_closed_interval, :right_closed_interval],
+
+      activerecord_utilities: [
+        :ar_where_in_range_exclusive, :ar_where_in_range_inclusive,
+        :ar_where_equal_to, :ar_where_not_equal_to,
+        :ar_where_larger_than, :ar_where_larger_than_or_equal_to, :ar_where_less_than, :ar_where_less_than_or_equal_to,
+        :ar_and_chain_5, :ar_and_chain_4, :ar_and_chain_3, :ar_and_chain_2, :ar_and_chain_1,
+        :ar_or_chain_5, :ar_or_chain_4, :ar_or_chain_3, :ar_or_chain_2, :ar_or_chain_1,
+      ]
     }.freeze
 
     def self.register(name)
