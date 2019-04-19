3d4f43ba9febd3a136bb22b82f510ca3794af4b5
TAGOMORI Satoshi <tagomoris@gmail.com>
2019-04-19 15:23:01 +0900

    add tests for placeholders newly added

--- test/test_dsl.rb
+++ test/test_dsl.rb
@@ -35,41 +35,60 @@ class DSLTest < ::Test::Unit::TestCase
   test 'placeholder_to_matcher_node returns matchers' do
     assert_kind_of Maccro::DSL::Value, Maccro::DSL.placeholder_to_matcher_node(ast_node(->(){ v1 }))
     assert_kind_of Maccro::DSL::Expression, Maccro::DSL.placeholder_to_matcher_node(ast_node(->(){ e1 }))
+
+    assert_kind_of Maccro::DSL::String, Maccro::DSL.placeholder_to_matcher_node(ast_node(->(){ s1 }))
+    assert_kind_of Maccro::DSL::Symbol, Maccro::DSL.placeholder_to_matcher_node(ast_node(->(){ y1 }))
+    assert_kind_of Maccro::DSL::Number, Maccro::DSL.placeholder_to_matcher_node(ast_node(->(){ n1 }))
+    assert_kind_of Maccro::DSL::RegularExpression, Maccro::DSL.placeholder_to_matcher_node(ast_node(->(){ r1 }))
   end
 
-  test 'ast_node_to_dsl_node' do
-    ast = proc_to_ast(->(){ 1 || v1 })
-    node = Maccro::DSL::ast_node_to_dsl_node(ast)
+  sub_test_case 'ast_node_to_dsl_node' do
+    test 'value' do
+      ast = proc_to_ast(->(){ 1 || v1 })
+      node = Maccro::DSL::ast_node_to_dsl_node(ast)
 
-    n = node
+      n = node
+      assert_equal :SCOPE, n.type
+      assert_kind_of Maccro::DSL::ASTNodeWrapper, n
 
-    assert_equal :SCOPE, n.type
-    assert_kind_of Maccro::DSL::ASTNodeWrapper, n
+      n = node.children[2]
+      assert_equal :OR, n.type
+      assert_kind_of Maccro::DSL::ASTNodeWrapper, n
+      assert_equal :LIT, n.children[0].type
+      assert_kind_of Maccro::DSL::ASTNodeWrapper, n.children[0]
+      assert_kind_of Maccro::DSL::Value, n.children[1]
+      assert_equal "v1", n.children[1].name
+    end
 
-    n = node.children[2]
+    test 'number or symbol' do
+      ast = proc_to_ast(->(){ n1 || y1 })
+      node = Maccro::DSL::ast_node_to_dsl_node(ast)
 
-    assert_equal :OR, n.type
-    assert_kind_of Maccro::DSL::ASTNodeWrapper, n
+      n = node
+      assert_equal :SCOPE, n.type
+      assert_kind_of Maccro::DSL::ASTNodeWrapper, n
 
-    assert_equal :LIT, n.children[0].type
-    assert_kind_of Maccro::DSL::ASTNodeWrapper, n.children[0]
-
-    assert_kind_of Maccro::DSL::Value, n.children[1]
-    assert_equal "v1", n.children[1].name
+      n = node.children[2]
+      assert_equal :OR, n.type
+      assert_kind_of Maccro::DSL::ASTNodeWrapper, n
+      assert_kind_of Maccro::DSL::Number, n.children[0]
+      assert_equal "n1", n.children[0].name
+      assert_kind_of Maccro::DSL::Symbol, n.children[1]
+      assert_equal "y1", n.children[1].name
+    end
   end
 
-  test 'matcher' do
-    node = Maccro::DSL.matcher('1 || v1')
+  sub_test_case 'matcher' do
+    test 'matcher' do
+      node = Maccro::DSL.matcher('1 || v1')
     
-    n = node
-
-    assert_equal :OR, n.type
-    assert_kind_of Maccro::DSL::ASTNodeWrapper, n
-
-    assert_equal :LIT, n.children[0].type
-    assert_kind_of Maccro::DSL::ASTNodeWrapper, n.children[0]
-
-    assert_kind_of Maccro::DSL::Value, n.children[1]
-    assert_equal "v1", n.children[1].name
+      n = node
+      assert_equal :OR, n.type
+      assert_kind_of Maccro::DSL::ASTNodeWrapper, n
+      assert_equal :LIT, n.children[0].type
+      assert_kind_of Maccro::DSL::ASTNodeWrapper, n.children[0]
+      assert_kind_of Maccro::DSL::Value, n.children[1]
+      assert_equal "v1", n.children[1].name
+    end
   end
 end
